02d915e6a98709b8de18968e887eb53a090c248b
Kokoa <lotusprncs@gmail.com>
2025-10-01 12:15:38 +0900

    Update Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,26 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import HeroBackground from "./HeroBackground";
 import HeroContents from "./HeroContents";
 
 const Hero: React.FC = () => {
-  return (
-    <section className="relative w-full h-screen z-10 flex items-center justify-center">
-      {/* 背景は固定 */}
-      <HeroBackground />
+  const [imgHeight, setImgHeight] = useState<number>(0);
 
-      {/* コンテンツはHeroセクション中央 */}
+  useEffect(() => {
+    const updateHeight = () => setImgHeight(window.innerHeight);
+    updateHeight();
+
+    window.addEventListener("resize", updateHeight);
+    return () => window.removeEventListener("resize", updateHeight);
+  }, []);
+
+  return (
+    <section
+      className="relative w-full z-10 flex items-center justify-center"
+      style={{ height: `${imgHeight}px` }}
+    >
+      <HeroBackground />
       <HeroContents />
     </section>
   );
